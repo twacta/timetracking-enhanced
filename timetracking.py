764ec731addebce21de7fd92fdbf5a69fcf33962
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2021-08-09 18:19:19 +0200

    :sparkles: Add month option

--- timetracking.py
+++ timetracking.py
@@ -4,65 +4,97 @@ from requests.auth import HTTPBasicAuth
 import json
 import datetime
 
-
-with open('config.json') as config_file:
+# tasks details: https://onedior.atlassian.net/browse/ONE-6650
+with open("config.json") as config_file:
     config = json.load(config_file)
 
-jiraUserName = config['JIRA_USERNAME']
-jiraApiToken = config['JIRA_API_TOKEN']
+jiraUserName = config["JIRA_USERNAME"]
+jiraApiToken = config["JIRA_API_TOKEN"]
 jiraApiHost = "https://onedior.atlassian.net/rest/api/3/"
-timePerIssue = config['timePerDay']
+timePerIssue = config["timePerDay"]
+useMonth = config["useMonth"]
+
 
 def yes_or_no(question):
-    reply = str(input(question+' (y/n): ')).lower().strip()
-    if reply[0] == 'y':
+    reply = str(input(question + " (y/n): ")).lower().strip()
+    if reply[0] == "y":
         return True
-    if reply[0] == 'n':
+    if reply[0] == "n":
         return False
     else:
         return yes_or_no("Uhhhh... please enter ")
 
+
+def getDays():
+    return getDaysOfThisMonth() if useMonth else getDaysOfThisWeek()
+
+
 def getDaysOfThisWeek():
     theday = datetime.date.today()
     weekday = theday.isoweekday()
-    start = theday - datetime.timedelta(days=weekday-1)
+    start = theday - datetime.timedelta(days=weekday - 1)
     dates = [start + datetime.timedelta(days=d) for d in range(5)]
     dates = [str(d) for d in dates]
     return dates
 
+
+def getDaysOfThisMonth():
+    theday = datetime.date.today()
+    start = datetime.date(theday.year, theday.month, 1)
+    end = datetime.date(
+        theday.year if theday.month < 12 else theday.year + 1,
+        theday.month + 1 if theday.month < 12 else 1,
+        1,
+    )
+    nbOfDay = (end - start).days
+    dates = [start + datetime.timedelta(days=d) for d in range(nbOfDay)]
+    dates = [str(d) for d in filter(lambda x: x.isoweekday() < 6, dates)]
+    return dates
+
+
 def addWorklogForOneIssueOneDay(issueNumber, startDate, timeSpent):
     auth = HTTPBasicAuth(jiraUserName, jiraApiToken)
-    headers = {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    }
-    payload = json.dumps({
-        "timeSpentSeconds": timeSpent * 3600, 
-        "started": startDate+"T09:30:00.000+0000"
-        })
+    headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    payload = json.dumps(
+        {
+            "timeSpentSeconds": timeSpent * 3600,
+            "started": startDate + "T09:30:00.000+0000",
+        }
+    )
     response = requests.request(
-      "POST",
-      jiraApiHost+'issue/'+issueNumber+'/worklog',
-      data=payload,
-      headers=headers,
-      auth=auth)
+        "POST",
+        jiraApiHost + "issue/" + issueNumber + "/worklog",
+        data=payload,
+        headers=headers,
+        auth=auth,
+    )
 
     return response.status_code
 
+
 def addWorkloadForAllDays():
-    for day in getDaysOfThisWeek():
+    for day in getDays():
         for issue in timePerIssue:
             if timePerIssue[issue] != 0:
-                print("Day " + day + ": " + "Adding " + str(timePerIssue[issue]) + " hours to issue " + issue)
+                print(
+                    "Day "
+                    + day
+                    + ": "
+                    + "Adding "
+                    + str(timePerIssue[issue])
+                    + " hours to issue "
+                    + issue
+                )
                 status = addWorklogForOneIssueOneDay(issue, day, timePerIssue[issue])
                 if status == 201:
-                    print('Success ✅')
-                else: 
-                    print('Something went wrong, you have to check on Jira... ❌')
+                    print("Success ✅")
+                else:
+                    print("Something went wrong, you have to check on Jira... ❌")
+
 
 print("💡 This will add predefined worklog on the following days")
-print(" " .join(map(str, getDaysOfThisWeek())))
+print(" ".join(map(str, getDays())))
 if yes_or_no("Do you confirm ?"):
     addWorkloadForAllDays()
-else :
+else:
     print("Ok aborting 😈")
